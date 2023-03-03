a2394b760e864cba8ef5ca6e0da3efa27da7ac41
redto0 <boccaccioalex@gmail.com>
2023-03-03 18:44:50 -0500

    final commit

--- Battle ship demo.js
+++ Battle ship demo.js
@@ -1,32 +1,50 @@
-function ship(name, length, X, Y, facingUp){
+println(" ");
+println("l + l");
+println("\nText goes here\n \n ");
+
+var ship = function (name, length, X, Y, facingUp) {
     this.name = name;
     this.length = length;
     this.x = X;
     this.y = Y;
     this.facingUp = facingUp;
-    this.hits = 0;
-}
+    this.hits = [];
+};
 
-function gridSqaure(X, Y){
+ship.prototype.addHit = function (spot) {
+    if (this.hits[0] === null) {
+        for (var i = 0; i > this.length; i++) {
+            this.hits.add(false);
+            this.hits[i] = false;
+        }
+    }
+    this.hits[spot] = true;
+};
+
+
+
+var gridSqaure = function (X, Y) {
     this.shotAt = false;
     this.shipIn = false;
     this.x = X;
     this.y = Y;
     this.ship = "";
     this.filler = color(90, 188, 237);
-    
-    function isShotIt(){
-        println(false);
-        if(this.shotAt !== true){
-            this.shotAt = true;
-            if(this.shipIn === true){
-                this.filler = color(255, 166, 0);
-            } else{ this.filler = color(255, 0, 0);}
-        }
+};
+gridSqaure.prototype.isShotIt = function () {
+    //println("Is shot ran!");
+    if (this.shotAt !== true) {
+        this.shotAt = true;
+        if (this.shipIn === true) {
+            this.filler = color(255, 166, 0);
+
+        } else { this.filler = color(255, 0, 0); }
     }
-}
+    //println(false);
+};
 
 //var place = new gridSqaure();
+var scene = 0;
 var gridYou = [];
 var gridOpposing = [];
 var gridSize = 12;
@@ -34,52 +52,99 @@ var stage = 1;
 var gridWithMouse = {
     i: 0,
     j: 0,
-    isYou: "",
-    setGrid: function(i,j, isYou){
+    isYou: false,
+    setGrid: function (i, j, isYou) {
         gridWithMouse.i = i;
         gridWithMouse.j = j;
         gridWithMouse.isYou = isYou;
     }
 };
+var battleship = new ship("Battleship", 4, 1, 1, true);
+var aircraftcarrier = new ship("aircraftcarrier", 5, 3, 3, false);
+var destroyer = new ship("destroyer", 2, 5, 5, true);
+var submarine = new ship("submarine", 5, 7, 7, true);
+var ptboat = new ship("ptboat", 5, 9, 9, true);
+var arrShips = [battleship, aircraftcarrier, destroyer, submarine];
+
+var assginShips = function (arrShip, grid) {
+    for (var i = 0; i < arrShip.length; i++) {
+        grid[arrShip[i].x][arrShip[i].y].ship = arrShip[i];
+        if (arrShip[i].facingUp === true) {
+            //for (i = 0; i < arrShip[i].ship.length; i++){
+            //    grid[arrShip[i].x + i][arrShip[i].y].shipIn = true;
+            //    grid[arrShip[i].x][arrShip[i].y].ship = arrShip[i];
+            //}
+
+        }
+    }
+    return grid;
+};
+
+gridSqaure.prototype.drawShip = function (ship) {
+    /**
+     * Only nessary to call for the first block of each ship. 
+     * 
+     * MAKE SURE TO ASSGIN TO EACH INDIVIDUAL CELL
+     * 
+     * xdd more to come forsure
+     * 
+     */
+    //println("Hello");
+    if (scene === 0) {
+        if (this.ship !== null) {
+            if (this.ship.facingUp === true) {
+                fill(199, 197, 171);
+                rect(this.x, this.y, gridSize, gridSize * this.ship.length);
+                //println(gridSize * this.ship.length);
+
+            } else {
+                fill(199, 197, 171);
+                rect(this.x, this.y, gridSize * this.ship.length, gridSize);
+
+            }
+        }
+    }
+};
+
 {
-for(var i = 0; i < 15; i++){
-    gridOpposing.push([]);
-    for(var j = 0; j < 15; j++){
-        gridOpposing[i].push(new gridSqaure(j*gridSize + 195, i*gridSize + 1 ));
+    for (var i = 0; i < 15; i++) {
+        gridOpposing.push([]);
+        for (var j = 0; j < 15; j++) {
+            gridOpposing[i].push(new gridSqaure(j * gridSize + 195, i * gridSize + 1));
+        }
+    }
+    for (var i = 0; i < 15; i++) {
+        gridYou.push([]);
+        for (var j = 0; j < 15; j++) {
+            gridYou[i].push(new gridSqaure(j * gridSize + 5, i * gridSize + 1));
+        }
     }
 }
-for(var i = 0; i < 15; i++){
-    gridYou.push([]);
-    for(var j = 0; j < 15; j++){
-        gridYou[i].push(new gridSqaure(j*gridSize + 5, i*gridSize + 1 ));
-    }
-}
-}
-function mouxeInBtn(gridSqaure, gridSize){
-    if(gridSqaure.x > mouseX - this.gridSize &&
+function mouxeInBtn(gridSqaure, gridSize) {
+    if (gridSqaure.x > mouseX - this.gridSize &&
         gridSqaure.x < mouseX &&
         gridSqaure.y > mouseY - this.gridSize &&
-        gridSqaure.y < mouseY)
-    {
+        gridSqaure.y < mouseY) {
         return true;
     } else {
         return false;
     }
 }
-function mouseInGrid(arr, gridSize){
-    for(var i = 0; i < arr.length; i++){
-        for(var j = 0; j < arr[i].length; j++){
+
+function mouseInGrid(arr, gridSize) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
             println(arr[i][j].x);
-            if(arr[i][j].x < mouseX + this.gridSize &&
-               arr[i][j].x > mouseX &&
-               arr[i][j].y < mouseY + this.gridSize &&
-               arr[i][j].y > mouseY
-               ){
-                    var returned = {
+            if (arr[i][j].x < mouseX + this.gridSize &&
+                arr[i][j].x > mouseX &&
+                arr[i][j].y < mouseY + this.gridSize &&
+                arr[i][j].y > mouseY
+            ) {
+                var returned = {
                     x: arr[i][j].x,
                     y: arr[i][j].y
-                    };
-                    return returned ;
+                };
+                return returned;
             }
         }
     }
@@ -87,48 +152,78 @@ function mouseInGrid(arr, gridSize){
 
 /*------------------------------------------------------------ */
 
-draw = function() {
+draw = function () {
     background(50, 56, 56);
     /*------------------------------------------------------------ */
-    {
-    for(var i = 0; i < gridOpposing.length; i++){
-        for(var j = 0; j < gridOpposing[i].length; j++){
-            fill(gridOpposing[i][j].filler);
-            rect(gridOpposing[i][j].x, gridOpposing[i][j].y, gridSize,gridSize);
+    {/*
+        for (var i = 0; i < gridOpposing.length; i++) {
+            for (var j = 0; j < gridOpposing[i].length; j++) {
+                fill(gridOpposing[i][j].filler);
+                rect(gridOpposing[i][j].x, gridOpposing[i][j].y, gridSize, gridSize);
+                gridOpposing[i][j].drawShip();
+            }
+        }*/
+        for (var i = 0; i < gridYou.length; i++) {
+            for (var j = 0; j < gridYou[i].length; j++) {
+                fill(gridYou[i][j].filler);
+                rect(gridYou[i][j].x, gridYou[i][j].y, gridSize, gridSize);
+                fill(gridOpposing[i][j].filler);
+                rect(gridOpposing[i][j].x, gridOpposing[i][j].y, gridSize, gridSize);
+            }
         }
-    }
-    for(var i = 0; i < gridYou.length; i++){
-        for(var j = 0; j < gridYou[i].length; j++){
-            fill(gridOpposing[i][j].filler);
-            rect(gridYou[i][j].x, gridYou[i][j].y, gridSize,gridSize);
+        for (var i = 0; i < gridYou.length; i++) {
+            for (var j = 0; j < gridYou[i].length; j++) {
+                gridYou[i][j].drawShip();
+                gridOpposing[i][j].drawShip();
+            }
         }
-    }
-    
-    for(var i = 0; i < gridOpposing.length; i++){
-        for(var j = 0; j < gridOpposing[i].length; j++){
-            if (mouxeInBtn(gridOpposing[i][j], gridSize) === true){
-                fill(255, 0, 0); 
-                gridWithMouse.setGrid(i, j, false);
-                rect(gridOpposing[i][j].x, gridOpposing[i][j].y, gridSize,gridSize);
+
+        for (var i = 0; i < gridOpposing.length; i++) {
+            for (var j = 0; j < gridOpposing[i].length; j++) {
+                if (mouxeInBtn(gridOpposing[i][j], gridSize) === true) {
+                    fill(255, 0, 0);
+                    gridWithMouse.setGrid(i, j, false);
+                    rect(gridOpposing[i][j].x, gridOpposing[i][j].y, gridSize, gridSize);
+                }
+            }
+        }
+        for (var i = 0; i < gridYou.length; i++) {
+            for (var j = 0; j < gridYou[i].length; j++) {
+                if (mouxeInBtn(gridYou[i][j], gridSize) === true) {
+                    fill(255, 0, 0);
+                    gridWithMouse.setGrid(i, j, true);
+                    //println("gridWithMouse works!");
+                    rect(gridYou[i][j].x, gridYou[i][j].y, gridSize, gridSize);
+                }
             }
         }
     }
-    for(var i = 0; i < gridYou.length; i++){
-        for(var j = 0; j < gridYou[i].length; j++){
-            if (mouxeInBtn(gridYou[i][j], gridSize) === true){
-                fill(255, 0, 0);
-                gridWithMouse.setGrid(i, j, true);
-                rect(gridYou[i][j].x, gridYou[i][j].y, gridSize,gridSize);
-            }
-        }
-    }
-    }
-    mouseClicked = function() {
-        if (gridWithMouse.isYou === false){
+
+    assginShips(arrShips, gridYou);
+
+    //gridYou[0][1].ship = battleship;
+    //gridYou[0][1].drawShip();
+    //gridYou[0][3].ship = aircraftcarrier;
+    //gridYou[0][3].drawShip();
+
+
+
+    mouseClicked = function () {
+        //println("isClicked");
+        if (gridWithMouse.isYou === false) {
+            println("isn't you");
             println(gridWithMouse.j + ":" + gridWithMouse.i);
-            println(gridOpposing[gridWithMouse.i][gridWithMouse.j].shotAt); //isShotIt();
-        }else if(gridWithMouse.isYou === true){
+            //println(gridOpposing[gridWithMouse.i][gridWithMouse.j].shotAt); //isShotIt();
+            gridOpposing[gridWithMouse.i][gridWithMouse.j].isShotIt();
+            println("endl");
+        } else if (gridWithMouse.isYou === true) {
+            println("is you");
+            println(gridWithMouse.j + ":" + gridWithMouse.i);
+            //println(gridYou[gridWithMouse.i][gridWithMouse.j].shotAt);
             gridYou[gridWithMouse.i][gridWithMouse.j].isShotIt();
+            println("endl");
+        } else {
+            println("chain is fail");
         }
     };
 };
--- Memory game.js.js
+++ Memory game.js.js
@@ -1,24 +1,24 @@
 frameRate(1);
 
-function b0x(X, Y, length, wdith, colour){
-    this.x = X; 
+function b0x(X, Y, length, wdith, colour) {
+    this.x = X;
     this.y = Y;
-    this.length = length; 
-    this.wdith = wdith; 
+    this.length = length;
+    this.wdith = wdith;
     this.colour = colour;
 }
 
-function mouxeInBtn(gridSqaure, gridSize){
-    if(gridSqaure.x > mouseX - gridSize &&
-        gridSqaure.x < mouseX &&
-        gridSqaure.y > mouseY - gridSize &&
-        gridSqaure.y < mouseY)
-    {
+var mouxeInBtn = function (x, y) {
+    if (x + 140 > mouseX &&
+        x < mouseX &&
+        y + 140 > mouseY &&
+        y < mouseY) {
+        //println(true);
         return true;
     } else {
         return false;
     }
-}
+};
 
 var box1 = new b0x(30, 30, 140, 140, color(255, 0, 0));
 var box2 = new b0x(230, 30, 140, 140, color(251, 255, 0));
@@ -27,19 +27,22 @@ var box4 = new b0x(230, 230, 140, 140, color(0, 255, 47));
 
 var doLightTrick = true;
 var rightClick = true;
-var arryLights = [1];
+var arryLights = [1, 3, 4, 2];
 var countier = 0;
 var stillPlaying = true;
 var hasFailed = false;
 
 
 draw = function () {
+    //println(countier);
+    //println(doLightTrick);
+    //println(arryLights);
     if (hasFailed === true) {
         background(255, 255, 255);
         textSize(30);
         text("You Failed!", 150, 200);
     }
-    if (stillPlaying === true) {
+    if (hasFailed === false) {
         if (countier > arryLights.length) {
             /**
              * this will only run once!
@@ -55,12 +58,15 @@ draw = function () {
                  * also, add code to reset the lights when done with the light trick. 
                  */
 
-                countier = 0;
-                arryLights.push(Math.ceil(Math.random() * 4));
+                countier = 1;
+                //arryLights.push(Math.ceil(Math.random() * 4));
                 doLightTrick = false;
             } else if (doLightTrick === false) {
-                countier = 0;
+                countier = 1;
+                arryLights.push(Math.ceil(Math.random() * 4));
                 doLightTrick = true;
+                fill(242, 87, 87);
+                rect(box1.x, 30, 140, 140);
                 /* make the ligt trick stop. 
                  * start the cycle for the buttons. 
                  * reset counter once. 
@@ -69,7 +75,7 @@ draw = function () {
         }
         background(255, 255, 255);
         fill(0, 0, 0);
-        text("score is " + countier + ".", 370, 390);
+        text("score is " + arryLights.length + ".", 338, 390);
         //println(box1.x);
         fill(box1.colour);
         rect(box1.x, 30, 140, 140);
@@ -79,60 +85,75 @@ draw = function () {
 
         fill(box3.colour);
         rect(box3.x, 230, 140, 140);
-        
+
         fill(box4.colour);
         rect(box4.x, box4.y, box4.length, box4.length);
 
         if (doLightTrick === true) {
-            switch (arryLights[countier]) {
-                case 1:
-                    box1.color = color(242, 87, 87);
-                    break;
-                case 2:
-                    box2.color = color(251, 255, 05);
-                    break;
-                case 3:
-                    box3.color = color(53, 56, 250);
-                    break;
-                case 4:
-                    box4.color = color(45, 250, 83);
+            if (Math.floor(countier) % countier === 0) {
+
+                switch (arryLights[countier - 1]) {
+                    case 1:
+                        fill(242, 87, 87);
+                        rect(box1.x, 30, 140, 140);
+                        break;
+                    case 2:
+                        fill(171, 171, 70);
+                        rect(box2.x, 30, 140, 140);
+                        break;
+                    case 3:
+                        fill(63, 65, 181);
+                        rect(box3.x, 230, 140, 140);
+                        break;
+                    case 4:
+                        fill(66, 166, 85);
+                        rect(box4.x, box4.y, box4.length, box4.length);
+                        break;
+                }
             }
-            countier++;
+            countier += 0.5;
         } else if (doLightTrick === false) {
-             
+
         }
 
     }
 
     mouseClicked = function () {
-            // called when ever the mouse is clicked
-        if (stillPlaying === true) {
-            if (doLightTrick === false) { //checks to see if the light running through cycle is happening. 
-                if (mouxeInBtn(box1.x, box1.y) ) {
-                    box1.color = color(242, 87, 87);
-                    if (arryLights[countier] === 1) {
+        //println("mouseclicked");
+        // called when ever the mouse is clicked
+        if (hasFailed === false) {
+            if (doLightTrick === false) {
+                //checks to see if the light running through cycle is happening. 
+                if (mouxeInBtn(box1.x, box1.y)) {
+                    //println("mouse in 0n2");
+                    fill(242, 87, 87);
+                    rect(box1.x, 30, 140, 140);
+                    if (arryLights[countier - 1] === 1) {
                         countier++;
                     } else {
                         hasFailed = true;
                     }
-                } else if (mouxeInBtn(box2.x, box2.y) ) {
-                    box2.color = color(251, 255, 05);
-                    if (arryLights[countier] === 2) {
+                } else if (mouxeInBtn(box2.x, box2.y)) {
+                    fill(171, 171, 70);
+                    rect(box2.x, 30, 140, 140);
+                    if (arryLights[countier - 1] === 2) {
                         countier++;
                     } else {
                         hasFailed = true;
                     }
-                } else if (mouxeInBtn(box3.x, box3.y) ) {
-                    box3.color = color(53, 56, 250);
-                    if (arryLights[countier] === 3) {
+                } else if (mouxeInBtn(box3.x, box3.y)) {
+                    fill(63, 65, 181);
+                    rect(box3.x, 230, 140, 140);
+                    if (arryLights[countier - 1] === 3) {
                         countier++;
                     } else {
                         hasFailed = true;
                     }
-                } else if (mouxeInBtn(box4.x, box4.y) ) {
-                    box4.color = color(45, 250, 83);
-                    if (arryLights[countier] === 4) {
-                         countier++;
+                } else if (mouxeInBtn(box4.x, box4.y)) {
+                    fill(66, 166, 85);
+                    rect(box4.x, box4.y, box4.length, box4.length);
+                    if (arryLights[countier - 1] === 4) {
+                        countier++;
                     } else {
                         hasFailed = true;
                     }
@@ -142,6 +163,6 @@ draw = function () {
             /* here will go the button to play again */
         }
     };
-    
-    
+
+
 };
